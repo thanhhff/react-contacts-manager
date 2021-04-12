2fe04e3fda7e5ebf5ceb4f86626864b5bfd38287
Thanh Nguyen <ec2-user@ip-172-31-2-149.ec2.internal>
2021-04-12 11:33:49 +0000

    Create app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React from 'react'
 import './styles/main.css';
 
 /* コンポーネント */
-import Todo from './components/Todo';
 
 function App() {
   return (
     <div className="container is-fluid">
-      <Todo />
+      
+      Hello 
     </div>
   );
 }
