67d0a1af0e5fb1526d5c833132953f6e406b6816
Thanh Nguyen <ec2-user@ip-172-31-2-149.ec2.internal>
2021-04-12 11:28:42 +0000

    Init

---
+++ src/components/TodoItem.js
@@ -0,0 +1,26 @@
+/* 
+  【TodoItemコンポーネント】
+　・Todoアイテムを表示する
+　・チェックボックスにチェックが入っているか管理する
+　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
+*/
+function TodoItem( {item, onClick} ) {
+  const handleChange = () => {
+    onClick(item)
+  }
+  
+  return (
+    <label className="panel-block">
+      <input 
+        type="checkbox"
+        checked={item.done}
+        onClick={handleChange}
+      />
+      <span className={item.done ? 'has-text-grey-light' : ''}>
+        {item.text}
+      </span>
+    </label>
+  );
+}
+
+export default TodoItem;
