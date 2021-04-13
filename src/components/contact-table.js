97094de260f47171ba9e6ede340738f8f45f8523
quanglinh2545 <quanglinh2545@gmail.com>
2021-04-13 15:38:01 +0700

    contact-table

--- src/components/contact-table.js
+++ src/components/contact-table.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext } from "react";
 import { Segment, Table, Button, Icon } from "semantic-ui-react";
-import { ContactContext } from "./contact-context";
+import { ContactContext } from "../context/contact-context";
 
 export default function ContactTable() {
+  // Subscribe to `contacts` state and access dispatch function
   const [state, dispatch] = useContext(ContactContext);
-
+  // Declare a local state to be used internally by this component
   const [selectedId, setSelectedId] = useState();
 
   const delContact = id => {
@@ -32,9 +33,35 @@ export default function ContactTable() {
   ));
 
   return (
-    // TODO: viết table 
-    <div>
-    Test
-    </div>
+    <Segment>
+      <Table celled striped selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Id</Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{rows}</Table.Body>
+        <Table.Footer fullWidth>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell colSpan="4">
+              <Button
+                floated="right"
+                icon
+                labelPosition="left"
+                color="red"
+                size="small"
+                disabled={!selectedId}
+                onClick={onRemoveUser}
+              >
+                <Icon name="trash" /> Remove User
+              </Button>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </Segment>
   );
 }
