32dbff9f5aeb761f9dbd1cc373a925ffecbb8b7a
maihuy <maihuysl1999@gmail.com>
2021-04-13 13:20:30 +0700

    add form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -19,10 +19,23 @@ export default function ContactForm() {
   };
 
   return (
-    // TODO: viết hàm để thêm Contacts 
-    <div>
-      Test
-    </div>
+    <Segment basic>
+      <Form onSubmit={onSubmit}>
+        <Form.Group widths="3">
+          <Form.Field width={6}>
+            <Input placeholder="名前" {...name} required />
+          </Form.Field>
+          <Form.Field width={6}>
+            <Input placeholder="メール" {...email} type="email" required />
+          </Form.Field>
+          <Form.Field width={4}>
+            <Button fluid primary>
+              新しいコンタクト
+            </Button>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+    </Segment>
   );
 }
 
